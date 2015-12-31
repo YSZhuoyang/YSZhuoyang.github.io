a634c872811c27e61050eede31b0c0efb0c0ea8f
YSZhuoyang <yusanggit@163.com>
2015-12-30 17:25:38 -0800

    Initial commit

---
+++ js/update.js
@@ -0,0 +1,13 @@
+function loadMyCVDoc()
+{
+  var xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+     document.getElementById("demo").innerHTML = xhttp.responseText;
+    }
+  };
+
+  xhttp.open("GET", "ajax_info.txt", true);
+  xhttp.send();
+}
