272d59bb053428bef314a9d79f80cfc4295dd944
oscar <yuszy@outlook.com>
2016-11-20 00:13:08 +1100

    Abandoned google material ui

--- src/projects/Proj.js
+++ src/projects/Proj.js
@@ -1,54 +1,19 @@
 
 import React from 'react';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
-
-//import './Proj.css';
-
-injectTapEventPlugin();
+import './Proj.css';
 
 export default class Proj extends React.Component {
 
-    static childContextTypes =
-    {
-        muiTheme: React.PropTypes.object
-    }
-
-    getChildContext()
-    {
-        return {
-            muiTheme: getMuiTheme()
-        }
-    }
-
     renderProj1() {
         return(
-            <Card>
-                <CardHeader
-                title="URL Avatar"
-                subtitle="Subtitle"
-                avatar="./img/pass.png"
-                />
-                <CardMedia
-                overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
-                >
-                <img src="./img/pass.png" alt="Project 1" />
-                </CardMedia>
-                <CardTitle title="Card title" subtitle="Card subtitle" />
-                <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-                </CardText>
-                <CardActions>
-                <FlatButton label="Action1" />
-                <FlatButton label="Action2" />
-                </CardActions>
-            </Card>
+            <div className="card">
+                <img src="./img/pass.png" alt="Avatar" />
+                <div className="desc">
+                    <h4><b>Proj 1</b></h4>
+                    <p>Proj 1 content</p>
+                </div>
+            </div>
         );
     }
 
