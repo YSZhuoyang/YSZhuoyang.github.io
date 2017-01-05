2c0e55d63333a7395098b67ee0b94690c7408748
oscar <yuszy@outlook.com>
2017-01-05 16:36:24 +1100

    Added content in my trace

--- src/trace/Trace.js
+++ src/trace/Trace.js
@@ -2,24 +2,47 @@
 import React from 'react';
 import Timeline from 'react-image-timeline';
 import 'react-image-timeline/dist/timeline.css';
+import './trace.css';
 
 const events = [{
-	"date": Date.parse("2013-05-15T07:00:00.000Z"),
-	"text": "Sed leo elit, pellentesque sit amet congue quis, ornare nec lorem. Phasellus tincidunt rhoncus magna,\neget elementum odio rutrum fermentum. Ut a justo lacus. Maecenas blandit molestie felis ac viverra. Pellentesque\nsagittis ligula neque, sit amet feugiat massa tempor sed. Duis id bibendum ex, pulvinar tincidunt ",
-	"title": "Berlin, Germany",
-	"imageUrl": "https://s3.amazonaws.com/aaron-cdn/react-image-timeline/berlin.jpg"
+	"date": Date.parse("2016-01-24T07:00:00.000Z"),
+	"text": "I really like Radvanska's style of playing.",
+	"title": "Australian Open, Melbourne",
+	"imageUrl": "./img/events/ausopen.jpg"
 }, {
-	"date": Date.parse("2013-08-14T07:00:00.000Z"),
-	"text": "Sed leo elit, pellentesque sit amet congue quis, ornare nec lorem. Phasellus tincidunt rhoncus magna,\neget elementum odio rutrum fermentum. Ut a justo lacus. Maecenas blandit molestie felis ac viverra. Pellentesque\nsagittis ligula neque, sit amet feugiat massa tempor sed. Duis id bibendum ex, pulvinar tincidunt orci. Curabitur\nid sem urna. Maecenas sed elit malesuada, cursus ligula ut, vestibulum lorem. Suspendisse vitae ultrices libero.\nMauris maximus, ligula vitae tincidunt scelerisque, ipsum magna euismod massa, eu vulputate enim est tempus sem.\nMaecenas id nibh vitae ante volutpat facilisis nec eget velit. Proin et ligula feugiat, auctor tellus sit amet,\naccumsan neque. Quisque porttitor lectus quis elit fermentum, a facilisis est suscipit. Integer consectetur dapibus\nnisi, ut lacinia enim vulputate vitae. Curabitur id diam mauris. Duis dictum, dolor at porttitor aliquet, justo libero\nmattis magna, eu pellentesque augue mauris eget erat. Pellentesque lacinia velit nec ullamcorper mollis. Pellentesque\nlacus tortor, m",
-	"title": "Chicago, Illinois",
-	"imageUrl": "https://s3.amazonaws.com/aaron-cdn/react-image-timeline/chicago.jpg"
-}];
+	"date": Date.parse("2015-12-31T07:00:00.000Z"),
+	"text": "Last night of 2015, St Kilda, Crown were full of people, waiting for the comming of 2016. Disappointingly, the firework lasted for 5 minites only",
+	"title": "New year's eve 2016, Melbourne",
+	"imageUrl": "./img/events/2016newyear.jpg"
+}, {
+	"date": Date.parse("2016-02-11T07:00:00.000Z"),
+	"text": "This is a brand new begining in 614 Queensberry. I love this new home",
+	"title": "614 Queensberry, Melbourne",
+	"imageUrl": "./img/events/firstday614.jpg"
+}, {
+	"date": Date.parse("2015-09-28T07:00:00.000Z"),
+	"text": "Took a rest in the middle break",
+	"title": "Belgrave, Melbourne",
+	"imageUrl": "./img/events/dandenong.jpg"
+}, {
+	"date": Date.parse("2016-02-07T07:00:00.000Z"),
+	"text": "First hanging out with new housemates in 614 Queensberry",
+	"title": "Southbank, Melbourne",
+	"imageUrl": "./img/events/haningoutwith614.jpg"
+}
+];
+
+const emptyfooter = (props) => {
+    return <div></div>;
+};
 
 export default class App extends React.Component {
-    
+
     render() {
         return (
-            <Timeline events={events} />
+			<div className="timeline-div">
+				<Timeline events={events} customFooter={emptyfooter}/>
+			</div>
         );
     }
 }
