9797b61e77e34f44fe90d77908ec0c9e8b2ca219
oscar <yuszy@outlook.com>
2017-06-04 22:48:55 +1000

    Swipeview style improved

--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -1,32 +1,16 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import SwipeViews from 'react-swipe-views';
 import {Tabs, Tab} from 'material-ui/Tabs';
 import { getMuiTheme } from 'material-ui/styles';
 import SwipeableViews from 'react-swipeable-views';
 
-import AboutMe from '../aboutMe/AboutMe.js';
-import Proj from '../projects/Proj.js';
-import Trace from '../trace/Trace.js';
+import AboutMe from '../aboutMe/AboutMe';
+import Proj from '../projects/Proj';
+import Trace from '../trace/Trace';
 
-// import 'react-swipe-views/lib/react-swipe-views.css';
 import './navbar.css';
 
-
-/*const NavBar = () => (
-    <SwipeViews>
-        <div title={<em id="tab">Trace</em>}>
-            <Trace />
-        </div>
-        <div title={<em id="tab">My Work</em>}>
-            <Proj />
-        </div>
-        <div title={<em id="tab">About Me</em>}>
-            <AboutMe />
-        </div>
-    </SwipeViews>
-);*/
 
 class NavBar extends Component {
 
@@ -34,7 +18,35 @@ class NavBar extends Component {
         super(props);
         this.state = {
             slideIndex: 0,
+            // Tab styles
+            tabContainerStyles: {
+                background: 'transparent'
+            },
+            inkBarStyles: {
+                background: '#ee8855',
+                height: '5px'
+            },
+            labelStyles: []
         };
+        this.lableStyleOptions = {
+            defaultLabelStyle: {
+                fontSize: 32,
+                fontStyle: 'italic',
+                fontFamily: 'Times New Roman',
+                color: '#eeeeddaa'
+            },
+            activeLabelStyle: {
+                fontSize: 32,
+                fontStyle: 'italic',
+                fontFamily: 'Times New Roman',
+                color: '#eeeedd'
+            }
+        };
+        this.labelStyleTemplates = [
+            this.lableStyleOptions.defaultLabelStyle,
+            this.lableStyleOptions.defaultLabelStyle,
+            this.lableStyleOptions.defaultLabelStyle
+        ];
     }
 
     handleChange = (value) => {
@@ -54,27 +66,24 @@ class NavBar extends Component {
     }
 
     render() {
-        const tabContainerStyles = {
-            background: 'transparent'
-        };
-
-        const labelStyle = {
-            fontSize: 32,
-            fontStyle: 'italic',
-            fontFamily: 'Times New Roman'
-        };
+        const newLableStyles = this.labelStyleTemplates.slice();
+        newLableStyles[this.state.slideIndex] =
+            this.lableStyleOptions.activeLabelStyle;
 
         return (
             <div>
                 <Tabs
                 onChange={this.handleChange}
                 value={this.state.slideIndex}
-                tabItemContainerStyle={tabContainerStyles}
+                tabItemContainerStyle={this.state.tabContainerStyles}
+                inkBarStyle={this.state.inkBarStyles}
                 >
-                    <Tab label="My Trace" style={labelStyle} value={0} />
-                    <Tab label="My Work" style={labelStyle} value={1} />
-                    <Tab label="About Me" style={labelStyle} value={2} />
+                    <Tab label="My Trace" style={newLableStyles[0]} value={0} />
+                    <Tab label="My Work" style={newLableStyles[1]} value={1} />
+                    <Tab label="About Me" style={newLableStyles[2]} value={2} />
                 </Tabs>
+
+                <br/>
 
                 <SwipeableViews
                 index={this.state.slideIndex}
