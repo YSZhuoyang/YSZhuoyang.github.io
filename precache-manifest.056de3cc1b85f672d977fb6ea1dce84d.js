9207120a9c45f5456de51c712b62ede411e3429f
Oscar Yu <yuszy@outlook.com>
2019-08-15 19:46:23 +1000

    updated email

---
+++ precache-manifest.056de3cc1b85f672d977fb6ea1dce84d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a7e30e135c3dfe1ef72abaead5a7b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3163bc4563762136575",
+    "url": "/static/css/main.1ab36059.chunk.css"
+  },
+  {
+    "revision": "deba5d2a26bf9183085c",
+    "url": "/static/js/2.024423ed.chunk.js"
+  },
+  {
+    "revision": "e3163bc4563762136575",
+    "url": "/static/js/main.d1c50f4c.chunk.js"
+  },
+  {
+    "revision": "0f1a1a3f05b4ac929536",
+    "url": "/static/js/runtime~main.f799d364.js"
+  },
+  {
+    "revision": "e84ef5c9fe2cf16db7870680f2943da4",
+    "url": "/static/media/background.e84ef5c9.jpeg"
+  }
+]);
