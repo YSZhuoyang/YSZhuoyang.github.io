e8d369c8efc8e8a462868ac11b4a5bb965befdc7
Oscar Yu <yuszy@outlook.com>
2019-08-15 20:53:19 +1000

    chore: cleanup

--- src/Status/Status.js
+++ src/Status/Status.js
@@ -1,28 +1,35 @@
+import React, { memo } from "react";
 
-import React, { Component } from 'react';
+import "./Status.css";
 
-import './Status.css';
+export default memo(props => {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+  const dateObj = new Date();
+  const day = dateObj.getDate();
+  const month = dateObj.getMonth();
 
-
-class Status extends Component {
-    
-    render() {
-        const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-        const dateObj = new Date();
-        const day = dateObj.getDate();
-        const month = dateObj.getMonth();
-
-        return (
-            <div className='status-div'>
-                <div className="date">{months[month] + " " + day}</div>
-                <a className="news" href="http://www.crunchyroll.com/anime-news/2016/12/01/rurouni-kenshin-manga-heads-north-for-new-spin-off">
-                <div>
-                Rurouni Kenshin: Hokkaido Arc Manga announced!
-                </div>
-                </a>
-            </div>
-        );
-    }
-}
-
-export default Status;
+  return (
+    <div className="status-div">
+      <div className="date">{months[month] + " " + day}</div>
+      <a
+        className="news"
+        href="http://www.crunchyroll.com/anime-news/2016/12/01/rurouni-kenshin-manga-heads-north-for-new-spin-off"
+      >
+        <div>Rurouni Kenshin: Hokkaido Arc Manga announced!</div>
+      </a>
+    </div>
+  );
+});
--- src/trace/Trace.js
+++ src/trace/Trace.js
@@ -1,23 +1,17 @@
+import React, { memo } from "react";
+import Timeline from "react-image-timeline";
+import events from "./Events.js";
+import "react-image-timeline/dist/timeline.css";
+import "./trace.css";
 
-import React from 'react';
-// import Timeline from './timeline/timeline';
-import Timeline from 'react-image-timeline';
-import events from './Events.js';
-// import './timeline/timeline.css';
-import 'react-image-timeline/dist/timeline.css';
-import './trace.css';
+const emptyfooter = props => <></>;
 
-
-const emptyfooter = (props) => {
-    return <div></div>;
-};
-
-export default class Trace extends React.Component {
-    render() {
-        return (
-			<div className="timeline-div">
-				<Timeline events={events} reverseOrder={true} customFooter={emptyfooter}/>
-			</div>
-        );
-    }
-}
+export default memo(props => (
+  <div className="timeline-div">
+    <Timeline
+      events={events}
+      reverseOrder={true}
+      customComponents={{ footer: emptyfooter }}
+    />
+  </div>
+));
