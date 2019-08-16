47d3df7edfb1216b8ce457b47c73a2e215ae7d8e
Oscar Yu <yuszy@outlook.com>
2019-08-16 23:07:52 +1000

    fix: fixed navbar styling issue

--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -1,101 +1,79 @@
+import React from "react";
+import styled from "styled-components";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import SwipeableViews from "react-swipeable-views";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
 
-import React from 'react';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import SwipeableViews from 'react-swipeable-views';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import AboutMe from "../aboutMe/AboutMe";
+import Trace from "../trace/Trace";
 
-import AboutMe from '../aboutMe/AboutMe';
-import ProjGridList from '../projects/Proj';
-import Trace from '../trace/Trace';
+const StyledTab = styled(Tab)`
+  && {
+    flex: 1;
+    font-size: 20px;
 
+    @media (max-width: 768px) {
+      font-size: 14px;
+    }
+  }
+`;
 
 const getStyles = theme => ({
-    tabRoot: {
-        flex: 1,
-        width: "33%",
-        maxWidth: 400,
-        minWidth: 72
-    },
-    indicator: {
-        background: "#aaaa44",
-        height: 4
-    },
-    label: {
-        [theme.breakpoints.down('sm')]: {
-            fontSize: 14
-        },
-        [theme.breakpoints.up('md')]: {
-            fontSize: 18
-        },
-        [theme.breakpoints.up('lg')]: {
-            fontSize: 28
-        }
-    }
+  indicator: {
+    background: "#aaaa44",
+    height: 4
+  }
 });
 
-class NavBar extends React.Component {
+class NavBar extends React.PureComponent {
+  state = {
+    value: 0
+  };
 
-    state = {
-        value: 0
-    };
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
 
-    handleChange = (event, value) => {
-        this.setState({ value });
-      };
+  handleChangeIndex = index => {
+    this.setState({ value: index });
+  };
 
-    handleChangeIndex = index => {
-        this.setState({ value: index });
-    };
+  render() {
+    const { classes } = this.props;
 
-    render() {
-        const { classes } = this.props;
+    return (
+      <div>
+        <Tabs
+          value={this.state.value}
+          onChange={this.handleChange}
+          classes={{
+            indicator: classes.indicator
+          }}
+          centered
+        >
+          <StyledTab label="My Trace" value={0} />
+          <StyledTab label="About Me" value={1} />
+        </Tabs>
 
-        return (
-            <div >
-                <Tabs
-                value={this.state.value}
-                onChange={this.handleChange}
-                classes={{
-                    indicator: classes.indicator
-                }}
-                centered
-                >
-                    <Tab
-                    classes={{
-                        root: classes.tabRoot,
-                        label: classes.label
-                    }} label="My Trace" value={0} />
-                    {/* <Tab
-                    classes={{
-                        root: classes.tabRoot,
-                        label: classes.label
-                    }} label="My Work" value={1} /> */}
-                    <Tab
-                    classes={{
-                        root: classes.tabRoot,
-                        label: classes.label
-                    }} label="About Me" value={2} />
-                </Tabs>
+        <br />
 
-                <br/>
-
-                <SwipeableViews
-                index={this.state.value}
-                onChangeIndex={this.handleChangeIndex}
-                >
-                    <Trace />
-                    <ProjGridList windowWidth={this.props.windowWidth} />
-                    <AboutMe />
-                </SwipeableViews>
-            </div>
-        );
-    }
+        <SwipeableViews
+          index={this.state.value}
+          onChangeIndex={this.handleChangeIndex}
+        >
+          <Trace />
+          {/* <ProjGridList windowWidth={this.props.windowWidth} /> */}
+          <AboutMe />
+        </SwipeableViews>
+      </div>
+    );
+  }
 }
 
 NavBar.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(getStyles)(NavBar);
