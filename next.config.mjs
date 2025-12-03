d46a0d557a1889de15f3eed120b2ff921ab9f2da
Oscar Yu <sangzhuoyangy@gmail.com>
2025-12-03 20:45:58 +1100

    3rd attemp

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,8 @@
-import { PHASE_PRODUCTION_BUILD } from 'next/constants.js';
-
-/**
- * @param {string} phase
- * @returns {Promise<import('next').NextConfig>}
- */
-export default async (phase) => {
-    const repo = 'YSZhuoyang.github.io';
-    const isProd = process.env.NODE_ENV === 'production';
-    const assetPrefix = isProd ? `/${repo}/` : undefined;
-    const basePath = isProd ? `/${repo}` : undefined;
-
-    /** @type {import('next').NextConfig} */
-    const nextConfig = {
-        output: 'export',
-        distDir: 'build',
-        assetPrefix,
-        basePath,
-    };
-
-    return nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    distDir: 'build',
+    trailingSlash: true,
 };
+
+export default nextConfig;
