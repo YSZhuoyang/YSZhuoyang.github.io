40238f18d5fbc574e9e46072c6422c358b803dac
Oscar Yu <sangzhuoyangy@gmail.com>
2025-12-03 20:27:25 +1100

    try fixing next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,23 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-    output: 'export',
-    distDir: 'build',
-};
+import { PHASE_PRODUCTION_BUILD } from 'next/constants.js';
 
-export default nextConfig;
+/**
+ * @param {string} phase
+ * @returns {Promise<import('next').NextConfig>}
+ */
+export default async (phase) => {
+    const repo = 'YSZhuoyang.github.io';
+    const isProd = phase === PHASE_PRODUCTION_BUILD;
+
+    const assetPrefix = isProd ? `/${repo}/` : undefined;
+    const basePath = isProd ? `/${repo}` : undefined;
+
+    /** @type {import('next').NextConfig} */
+    const nextConfig = {
+        output: 'export',
+        distDir: 'build',
+        assetPrefix: assetPrefix,
+        basePath: basePath,
+    };
+
+    return nextConfig;
+};
